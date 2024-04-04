f27d800c33e61769cf425cfb533df96e4ff88c1a
António Manuel <antoniomanuel.iot@outlook.com>
2024-04-04 14:51:38 +0100

    client |connection implementation

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,5 +1,10 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DeleteItemCommand, DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+
+const responseOk = {
+  statusCode: 200,
+  body: "",
+};
 
 const dynamodbClient = new DynamoDBClient({});
 const clientsTable = process.env["CLIENTS_TABLE_NAME"] || ""
@@ -13,6 +18,7 @@ const body = event.body || "";
     case "$connect":
       return handleConnect(connectionId);
     case "$disconnect":
+      return handleDisconnect(connectionId);
     case "msg":
   }
 
@@ -34,10 +40,20 @@ const handleConnect = async (connectionId: string): Promise<APIGatewayProxyResul
     }),
   );
 
-  return {
-    statusCode: 200,
-    body: "",
-  }
+  return responseOk;
 };
-const handleDisconnect = async (connectionId: string): Promise<APIGatewayProxyResult> => {};
+const handleDisconnect = async (connectionId: string): Promise<APIGatewayProxyResult> => {
+  await dynamodbClient.send(
+    new DeleteItemCommand({
+        TableName: clientsTable, 
+        Key: {
+          connectionId: {
+            S: connectionId,
+          },
+        },
+    }),
+  );
+
+  return responseOk;
+};
 const handleMsg = async (thisConnectionId: string): Promise<APIGatewayProxyResult> => {};
