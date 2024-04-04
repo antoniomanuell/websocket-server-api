52398fae7b7da8c552153440478c0f0615d41148
António Manuel <antoniomanuel.iot@outlook.com>
2024-04-04 16:25:44 +0100

    aws ws server - pass the msg implementation

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,5 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { DeleteItemCommand, DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DeleteItemCommand, DynamoDBClient, PutItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { ApiGatewayManagementApi, GoneException } from "@aws-sdk/client-apigatewaymanagementapi"
+import { TextEncoder } from "util";
 
 const responseOk = {
   statusCode: 200,
@@ -7,7 +9,11 @@ const responseOk = {
 };
 
 const dynamodbClient = new DynamoDBClient({});
-const clientsTable = process.env["CLIENTS_TABLE_NAME"] || ""
+const apiGatewayManagementApi = new ApiGatewayManagementApi({
+  endpoint: process.env["WSSAPIGATEWAYENDPOINT"],
+});
+const clientsTable = process.env["CLIENTS_TABLE_NAME"] || "";
+const textEncoder = new TextEncoder();
 
 export const hendle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
 const connectionId = event.requestContext.connectionId as string
@@ -20,6 +26,7 @@ const body = event.body || "";
     case "$disconnect":
       return handleDisconnect(connectionId);
     case "msg":
+      return handleMsg(connectionId, body);
   }
 
   return {
@@ -56,4 +63,39 @@ const handleDisconnect = async (connectionId: string): Promise<APIGatewayProxyRe
 
   return responseOk;
 };
-const handleMsg = async (thisConnectionId: string): Promise<APIGatewayProxyResult> => {};
+const handleMsg = async (thisConnectionId: string, body: string): Promise<APIGatewayProxyResult> => {
+const output = await dynamodbClient.send(
+    new ScanCommand({
+        TableName: clientsTable, 
+    }),
+  );
+
+  if (output.Count && output.Count > 0) {
+    for (const item of output.Items || []) {
+      if (item["connectionID"].S !== thisConnectionId) {
+        await sendMessage(item["connectionID"].S as string, body);
+      }
+    }
+  } else {
+    await sendMessage(thisConnectionId, JSON.stringify({ action:"msg",type: "warning",body:"no recipient" }));
+  return responseOk;
+  };
+
+  
+};
+
+const sendMessage = async (connectionId: string, body: string) => {
+  try {
+    await apiGatewayManagementApi.postToConnection({
+      ConnectionId: connectionId,
+      Data: textEncoder.encode(body),
+    });
+  } catch (e) {
+    if (e instanceof GoneException){
+      await handleDisconnect(connectionId);
+      return;
+    }
+
+    throw e;
+  }
+};
